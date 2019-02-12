29fa1e7bec1a56d78520faaf4902ccce93f83969
Kuyul <peyopeyo92@gmail.com>
2019-02-12 21:58:40 +1300

    Obstacle Change

---
+++ Assets/Scripts/ObstacleTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleTrigger : MonoBehaviour
+{
+    public PopupObstacle Obstacle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "player")
+        {
+            Obstacle.Triggered = true;
+        }
+    }
+}
---
+++ Assets/Scripts/PopupObstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupObstacle : MonoBehaviour
+{
+    public float MoveSpeed = 2.0f;
+    public float MoveTowardsYPos;
+    public bool Triggered = false; //set to true from obstacle trigger script
+
+    private Vector3 MoveToPosition;
+
+    private void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(0, LevelControl.instance.ObstacleAngularVel, 0);
+        MoveToPosition = new Vector3(transform.position.x, transform.position.y + MoveTowardsYPos, transform.position.z);
+    }
+
+    void Update()
+    {
+        if (Triggered)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, MoveToPosition, MoveSpeed * Time.deltaTime);
+        }
+    }
+}
