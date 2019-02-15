2095dd181ac83b4ea953c5fbf7c29f1da41a5813
Kuyul <peyopeyo92@gmail.com>
2019-02-15 15:21:15 +1300

    Road Zoning

---
+++ Assets/Scripts/RoadScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadScript : MonoBehaviour
+{
+    public GameObject[] EvenRoads;
+    public GameObject[] OddRoads;
+
+    public Material[] EvenMaterials;
+    public Material[] OddMaterials;
+
+    private void Start()
+    {
+        var lvl = LevelControl.instance.GetCurrentLevel();
+        if (EvenMaterials.Length > 0)
+        {
+            var index = (lvl - 1) % EvenMaterials.Length;
+
+            for (int i = 0; i < EvenRoads.Length; i++)
+            {
+                EvenRoads[i].GetComponent<MeshRenderer>().material = EvenMaterials[index];
+                OddRoads[i].GetComponent<MeshRenderer>().material = OddMaterials[index];
+            }
+        }
+    }
+}
